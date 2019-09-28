cdbad17aa8c3f262d984cabbcb206346628c03bb
CSmith <CSmith14@pm.me>
2019-09-28 03:18:45 -0600

    change packagefile > config.vscode to array

--- bin/install.js
+++ bin/install.js
@@ -1,17 +1,18 @@
 const { resolve, join } = require( 'path' )
 const execa = require( 'execa' )
-
 const packRoot = resolve( __filename, '../../' )
 const { name, version, config } = require( join( packRoot, 'package.json' ) )
+const vscExe = config && config.vscode ? config.vscode : [ 'code' ]
 
-const vscExe = config && config.vscode ? config.vscode : 'code'
+vscExe.forEach( ( exe ) => install( exe ) )
 
-console.log( `
+function install ( exe ) {
+	console.log( `
 Installing ${name}, version ${version} ...
-` )
-
-const cmd = execa.commandSync( `${vscExe} --install-extension ${name}-${version}.vsix`, {
-	cwd: packRoot
-} )
-
-console.log( cmd.stdout )
+	` )
+	const command = `${exe} --install-extension ${name}-${version}.vsix`
+	const cmd = execa.commandSync( command, {
+		cwd: packRoot
+	} )
+	console.log( cmd.stdout )
+}
